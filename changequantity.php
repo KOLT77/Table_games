150385e33fad596c788e86ca80dddc753655ac57
KOLT77 <RoLaN100@yandex.ru>
2017-06-02 19:34:01 +0700

    Update changequantity.php

--- changequantity.php
+++ changequantity.php
@@ -3,7 +3,7 @@ session_start();
 if (isset($_SESSION['login']) && isset($_SESSION['adminmode'])) {
     echo header('Location:index.php');
 } else {
-	$productnumber = '';
+	$changequantity = '';
     for ($i = 0; $i < $_SESSION['basketcounter']; $i++) {
         if  (!empty($_GET['changequantity' . $i] == '-') && $_SESSION['quantity' . $i] > 1) {
             $_SESSION['quantity' . $i]--;

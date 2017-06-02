83cab5481830ba3df4a0685e259ebb383f150377
KOLT77 <RoLaN100@yandex.ru>
2017-06-02 19:32:20 +0700

    Update changequantity.php

--- changequantity.php
+++ changequantity.php
@@ -3,14 +3,21 @@ session_start();
 if (isset($_SESSION['login']) && isset($_SESSION['adminmode'])) {
     echo header('Location:index.php');
 } else {
+	$productnumber = '';
     for ($i = 0; $i < $_SESSION['basketcounter']; $i++) {
-        if ($_GET['changequantity' . $i] == '-' && $_SESSION['quantity' . $i] > 1) {
+        if  (!empty($_GET['changequantity' . $i] == '-') && $_SESSION['quantity' . $i] > 1) {
             $_SESSION['quantity' . $i]--;
             break;
         }
-        if ($_GET['changequantity' . $i] == '+') {
+		else {
+            continue;
+        }
+        if (!empty($_GET['changequantity' . $i] == '+') {
             $_SESSION['quantity' . $i]++;
             break;
+        }
+		else {
+            continue;
         }
     }
     echo '<script>location.href="', $_SERVER['HTTP_REFERER'], '"</script>';

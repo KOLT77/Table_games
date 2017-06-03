a009c4af38226b642f69346f2b5bc63f602df045
KOLT77 <RoLaN100@yandex.ru>
2017-06-04 05:43:11 +0700

    Update register.php

--- register.php
+++ register.php
@@ -1,43 +1,43 @@
 <?require 'head.php';?>
-   <body>
-      <div class="main-block">
-         <div class="container paper">
-            <?require 'header.php';?>
-            <div id="main">
-               <div id="catalog">
-                  <h2><a class="post_ttl">УСПЕШНАЯ РЕГИСТРАЦИЯ</a></h2>
-                  <div id="basket" style="background:none;">
-                     <?php
-						if (isset($_SESSION['login']))
-						{
-							echo '<script>location.href="index.php"</script>';
-						}
-						else
-						{
-						$login     = htmlspecialchars($_POST['login']);
-						$password = htmlspecialchars($_POST['password']);
-						$logins    = file('files/logins3Fd2D093F23d5FQ2O2.txt', FILE_IGNORE_NEW_LINES);
-						file_put_contents('files/logins3Fd2D093F23d5FQ2O2.txt', $login . "\n", FILE_APPEND);
-						file_put_contents('files/passwords9Y79Hqyd83JNBX8Q12.txt', $password . "\n", FILE_APPEND);
-						for ($i=0; $i<count($logins); $i++)
-						{
-							if ($login==$logins[$i])
-							{
-								$_SESSION['login_already_exist']=true;
-								echo '<script>location.href="registration.php"</script>';
-								exit;
-							}
-						}
-						echo '<p align="center" style="font-size:28px;color:#fff;"><b>Регистрация прошла успешно! Теперь вы можете авторизоваться(в шапке сайта)!</b></p>';
-						}
-						?>
-                  </div>
+<body>
+   <div class="main-block">
+      <div class="container paper">
+         <?require 'header.php';?>
+         <div id="main">
+            <div id="catalog">
+               <h2><a class="post_ttl">УСПЕШНАЯ РЕГИСТРАЦИЯ</a></h2>
+               <div id="basket" style="background:none;">
+                  <?php
+                     if (isset($_SESSION['login']))
+                     {
+                     	echo '<script>location.href="index.php"</script>';
+                     }
+                     else
+                     {
+                     $login     = htmlspecialchars($_POST['login']);
+                     $password = htmlspecialchars($_POST['password']);
+                     $logins    = file('files/logins3Fd2D093F23d5FQ2O2.txt', FILE_IGNORE_NEW_LINES);
+                     file_put_contents('files/logins3Fd2D093F23d5FQ2O2.txt', $login . "\n", FILE_APPEND);
+                     file_put_contents('files/passwords9Y79Hqyd83JNBX8Q12.txt', $password . "\n", FILE_APPEND);
+                     for ($i=0; $i<count($logins); $i++)
+                     {
+                     	if ($login==$logins[$i])
+                     	{
+                     		$_SESSION['login_already_exist']=true;
+                     		echo '<script>location.href="registration.php"</script>';
+                     		exit;
+                     	}
+                     }
+                     echo '<p align="center" style="font-size:28px;color:#fff;"><b>Регистрация прошла успешно! Теперь вы можете авторизоваться(в шапке сайта)!</b></p>';
+                     }
+                     ?>
                </div>
             </div>
-            <div class="clear-both"></div>
          </div>
+         <div class="clear-both"></div>
       </div>
-      <?require 'footer.php';?>
-      </div>
-   </body>
+   </div>
+   <?require 'footer.php';?>
+   </div>
+</body>
 </html>
